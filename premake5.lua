c306943d651bb13d549a8382db0b2f4a38cc3802
DariusRadu <radu.va.darius@student.utcluj.ro>
2025-02-02 22:42:59 +0200

    Fix premake script

--- premake5.lua
+++ premake5.lua
@@ -20,23 +20,17 @@ project "GLFW"
     }
 
     filter "system:windows"
-        buildoptions { "-std=c11", "-lgdi32" }
         systemversion "latest"
         staticruntime "On"
 
-        files {
-            "src/win32_init.c",
-            "src/win32_joystick.c",
-            "src/win32_monitor.c",
-            "src/win32_time.c",
-            "src/win32_thread.c",
-            "src/win32_window.c",
-            "src/wgl_context.c",
-            "src/egl_context.c",
-            "src/osmesa_context.c"
-        }
+        files { "src/**.c" }
 
         defines { "_GLFW_WIN32", "_CRT_SECURE_NO_WARNINGS" }
 
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+    filter { "configurations:Release" }
+        runtime "Release"
+        optimize "On"
+
+    filter { "configurations:Debug" }
+        runtime "Debug"
+        symbols "On"
