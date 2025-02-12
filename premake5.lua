9833c6d1eb3af42293ae9d7b784cde66913dbc4b
DariusRadu <radu.va.darius@student.utcluj.ro>
2025-02-12 20:52:06 +0200

    Update lua script to avoid importing all files on windows builds

--- premake5.lua
+++ premake5.lua
@@ -3,6 +3,7 @@ outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 project "GLFW"
     kind "StaticLib"
     language "C"
+    staticruntime "On"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-temp/" .. outputdir .. "/%{prj.name}")
@@ -16,21 +17,42 @@ project "GLFW"
         "src/input.c",
         "src/monitor.c",
         "src/vulkan.c",
-        "src/window.c"
+        "src/window.c",
+        "src/platform.c",
+        "src/posix_module.c",
+        
+        -- Not sure if these should be windows specific. 
+        -- Wait until linux support is added.
+        "src/null_platform.h",
+        "src/null_init.c",
+        "src/null_joystick.h", 
+        "src/null_joystick.c", 
+        "src/null_monitor.c",
+        "src/null_window.c",
     }
 
     filter "system:windows"
         systemversion "latest"
-        staticruntime "On"
 
-        files { "src/**.c" }
+        files { 
+            "src/win32_init.c", 
+            "src/win32_joystick.c", 
+            "src/win32_module.c", 
+            "src/win32_monitor.c", 
+            "src/win32_time.c", 
+            "src/win32_thread.c", 
+            "src/win32_window.c", 
+            "src/wgl_context.c", 
+            "src/egl_context.c", 
+            "src/osmesa_context.c",
+        }
 
         defines { "_GLFW_WIN32", "_CRT_SECURE_NO_WARNINGS" }
-
-    filter { "configurations:Release" }
-        runtime "Release"
-        optimize "On"
 
     filter { "configurations:Debug" }
         runtime "Debug"
         symbols "On"
+
+    filter { "configurations:Release" }
+        runtime "Release"
+        optimize "On"
